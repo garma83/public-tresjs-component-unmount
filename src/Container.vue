08f3be15f1eb4996d1f5ac80890fdacc2f3961da
garma <61622461+garma83@users.noreply.github.com>
2026-01-26 17:40:20 +0100

    d

--- src/Container.vue
+++ src/Container.vue
@@ -1,6 +1,18 @@
 <script lang="ts" setup>
 import MyObject from './MyObject.vue'
+import { onMounted, onUnmounted } from 'vue'
+
+onMounted(() => {
+    console.log("Container mounted")
+})
+onUnmounted(() => {
+    console.log("Container onUnmounted")
+})
 </script>
 <template>
     <MyObject />
+    <!-- <TresMesh>
+        <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+        <TresMeshLambertMaterial :color="'#ff0000'" />
+    </TresMesh> -->
 </template>
